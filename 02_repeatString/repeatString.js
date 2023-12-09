21570cd36135c05520059b43872688e73fee5928
Tarun Menon <t3snake@gmail.com>
2023-12-09 14:02:16 +0530

    Finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, timesToRepeat) {
+    if (timesToRepeat < 0){
+        return 'ERROR';
+    }
+    let newStr = "";
+    for(i=0; i<timesToRepeat; i++){
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
