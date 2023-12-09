eb55453202e3156ec029a43ea65cad62e2580914
Tarun Menon <t3snake@gmail.com>
2023-12-09 16:03:36 +0530

    Finish exercises 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = '';
+    for(char of str){
+        newStr = char + newStr;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    if(arguments.length <= 1) return arr;
+    for (i=1; i<arguments.length; i++){
+        let index = arr.indexOf(arguments[i]);
+        if (index<0) continue;
+        arr.splice(index, 1);
+    }
+    
+    return arr;
+    
 };
 
 // Do not edit below this line
