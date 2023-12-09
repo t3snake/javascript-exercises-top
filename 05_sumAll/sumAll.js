78c635a48996f503710d58a1f96d2ce5e7df4af7
Tarun Menon <t3snake@gmail.com>
2023-12-09 16:39:22 +0530

    Finish exercise 5, 6 and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start<0 || end<0) return 'ERROR';
+    if(isNaN(start) || isNaN(end) || typeof start === "string" || typeof end === "string") return 'ERROR';
+    if(start>end){
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
 
+    sum = 0;
+    for(i=start; i<=end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
