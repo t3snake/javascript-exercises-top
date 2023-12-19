5ee5571587b773985240fa133afee167089eb783
t3snake <t3snake@gmail.com>
2023-12-20 00:43:04 +0530

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce( (prev, cur) =>{
+        if (!("yearOfDeath" in cur)) cur["yearOfDeath"] = new Date().getFullYear();
+        if (!("yearOfDeath" in prev)) prev["yearOfDeath"] = new Date().getFullYear();
+        return prev.yearOfDeath - prev.yearOfBirth > cur.yearOfDeath - cur.yearOfBirth ? prev : cur;
+    }, people[0]);
 };
 
 // Do not edit below this line
