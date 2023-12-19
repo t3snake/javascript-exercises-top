e2f04855eb1b029ac5a4b495ee8b3a6b924519e1
t3snake <t3snake@gmail.com>
2023-12-19 23:42:45 +0530

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const summ = arr.reduce((sum, item) => sum + item, 0);
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const product = arr.reduce( (product, item) => product*item, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let ans = 1;
+	for (let i=0; i<b; i++) {
+    ans *= a;
+  }
+  return ans;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) return 1;
+  return n * factorial(n-1);
 	
 };
 
