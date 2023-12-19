b42b5d60e15a756deb53207e73c1ba7bc4661ce3
t3snake <t3snake@gmail.com>
2023-12-20 00:23:41 +0530

    Finis exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n1) {
+    let n = parseInt(n1);
+    let prev = 1;
+    let next = 1;
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 1;
+    for (let i=3; i<=(n); i++) {
+        let tmp = next;
+        next = next + prev;
+        prev = tmp;
+        if (i ===  n) {
+            return next;
+        }
 
+    }
 };
 
 // Do not edit below this line
