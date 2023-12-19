ce1db87265aa90cbfc5c86166b4203f9517e21cf
t3snake <t3snake@gmail.com>
2023-12-20 00:07:45 +0530

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (pstring) {
+    let array = Array.from(pstring);
+    let punc = [" ", ",", "!", "."];
+    let filteredArray = array.filter( (item) => !punc.find(i => i === item) );
+    console.log(filteredArray);
+    return filteredArray.slice().reverse().every( (value, index) => value.toUpperCase() === filteredArray[index].toUpperCase());
 };
 
 // Do not edit below this line
